05e81c1a784a07f9b4211641aefc9acf844711cb
天衡 <13265317640@163.com>
2021-11-04 19:25:48 +0800

    feat: 增加go知识

--- .vitepress/config/modules/go.js
+++ .vitepress/config/modules/go.js
@@ -14,6 +14,17 @@ const GoConfig = {
       text: '基本类型',
       link: baseUrl + '/基础/基本类型',
     },
+    {
+      text: '数组',
+      link: baseUrl + '/基础/数组',
+    },
+    { text: '切片', link: baseUrl + '/基础/切片' },
+    { text: '指针', link: baseUrl + '/基础/指针' },
+    { text: 'Map', link: baseUrl + '/基础/map' },
+    { text: '结构体', link: baseUrl + '/基础/结构体' },
+    { text: '流程控制', link: baseUrl + '/基础/流程控制' },
+    { text: '函数与方法', link: baseUrl + '/基础/函数' },
+    { text: '面向对象', link: baseUrl + '/基础/面向对象' },
   ],
 };
 
